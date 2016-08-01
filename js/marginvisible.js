08304e79df57be01366963e488d10cc8003b4f7b
jerome13011 <fanniojerome@free.fr>
2016-08-01 12:25:06 +0200

    ajout site entier gh-pages

---
+++ js/marginvisible.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ // hides the slickbox as soon as the DOM is ready
+ // shows the slickbox on clicking the noted link
+$(' button ').click(function() {
+$('.unputdevice').toggle('slow');
+return false;
+  });
+    });
